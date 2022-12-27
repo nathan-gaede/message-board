fc1be94e5038a7d2d5f168815b4f1752b16be410
Nathan Gaede <n.d.gaede@gmail.com>
2022-12-27 16:34:33 -0600

    Installed dependencies, material UI, created database.

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,7 +28,7 @@ else {
     pool = new pg.Pool({
         host: 'localhost',
         port: 5432,
-        database: 'prime_app',   // 	💥 Change this to the name of your database!
+        database: 'message-board',   // 	💥 Change this to the name of your database!
     });
 }
 
